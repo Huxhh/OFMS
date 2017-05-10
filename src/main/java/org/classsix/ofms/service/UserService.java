c042a6950aec254a8c250bed87aa89e6dff95853
jianglilinx <172045149@qq.com>
2017-05-10 23:02:24 +0800

    2017.5.10

---
+++ src/main/java/org/classsix/ofms/service/UserService.java
@@ -0,0 +1,14 @@
+package org.classsix.ofms.service;
+
+import org.classsix.ofms.domin.User;
+import org.classsix.ofms.status.UserStatus;
+
+/**
+ * Created by jiang on 2017/5/3.
+ * 面向运气，面向心情，面向Bug。
+ */
+public interface UserService {
+    UserStatus confirmLogin(String userName, String password);
+    UserStatus addUser(User user);
+    String findUser(String userName, String tel) throws Exception;
+}
