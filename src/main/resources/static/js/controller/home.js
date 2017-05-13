c00e8fba1cf64fff0de61588f69aa63d3dc792eb
hanfeng <1024290349@qq.com>
2017-05-14 01:37:15 +0800

    修改添加轮播器

--- src/main/resources/static/js/controller/home.js
+++ src/main/resources/static/js/controller/home.js
@@ -1,14 +1,18 @@
 app.controller('home', ['$scope', function ($scope) {
 	$scope.hotPage = 0;
 	$scope.hotList = [];
-	$scope.recommondPage = 0;
 	$scope.recommondList = [];
+	// $scope.recommendRunList = [];
 	$scope.newPage = 0;
 	$scope.newList = [];
 	// 页码列表
     $scope.page_num = [];
     // 当前页数
     $scope.isActive = 0;
+    $scope.uid = 1;
+    //轮播flag
+    $scope.scrollerFlag = true;
+    $scope.scrollerIndex = 0;
 }]);
 app.directive('homeDirective', ['request', '$state', function (request, $state) {
 	return {
@@ -17,8 +21,6 @@ app.directive('homeDirective', ['request', '$state', function (request, $state) 
 			// 分类显示
 			scope.$emit('classify_change', true);
 			//directive作用域内全局变量
-			var func;//用于随机切换电影的函数
-			var page;//用于随机切换电影的页数
 			var ran;//随机数
 			//directive函数
 			function get_page(url, type, callback) {
@@ -26,13 +28,9 @@ app.directive('homeDirective', ['request', '$state', function (request, $state) 
 					console.error('url needed!!!');
 					return false;
 				}
-				request.get(url + '/0/15', function (res) {
+				request.get(url, function (res) {
 					if (res.code == 0) {
-						if(type == 1) {
-							scope.hotPage = res.body.totalPages;
-						} else if (type == 2) {
-							scope.recommondPage = res.body.totalPages;
-						}
+						scope.hotPage = res.body.totalPages;
 						callback(type);
 					} else {
 						request.pop_up(res.msg);
@@ -52,14 +50,11 @@ app.directive('homeDirective', ['request', '$state', function (request, $state) 
 					}
 				})
 			}
-			function get_guess(page, size) {
-				page = page || 0;
-				size = size || 15;
-				request.get('/recommend/buycount/' + page +  '/' + size,
+			function get_guess() {
+				request.get('/recommend/favor/' + scope.uid,
 					function (res) {
 						if (res.code == 0) {
-							scope.recommondList = res.body.content;
-							scope.recommondPage = res.body.totalPages;
+							scope.recommondList = res.body;
 						} else {
 							request.pop_up(res.msg);
 					}
@@ -69,8 +64,8 @@ app.directive('homeDirective', ['request', '$state', function (request, $state) 
 				scope.isActive = go_page;
                 page = go_page || 0;
 				page = page || 0;
-				size = size || 48;
-				request.get('/recommend/buycount/' + page +  '/' + size,
+				size = size || 32;
+				request.get('/recommend/buycount/' + page + '/' + size,
 					function (res) {
 						if (res.code == 0) {
 							scope.newList = res.body.content;
@@ -83,7 +78,7 @@ app.directive('homeDirective', ['request', '$state', function (request, $state) 
 	                            }
 	                        }
 						} else {
-							request.pop_up(res.msg);
+						request.pop_up(res.msg);
 					}
 				})
 			}
@@ -97,23 +92,19 @@ app.directive('homeDirective', ['request', '$state', function (request, $state) 
 			}
 			scope.random_change = function (type) {
 				ran = Math.random();
-				if (type == 1) {
-					func = get_recommend;
-					page = scope.hotPage;
-				} else {
-					func = get_guess;
-					page = scope.recommondPage;
-				}
 				if (ran > 0.5) {
-					func(Math.ceil(ran * page), 15);
+					get_recommend(Math.ceil(ran * scope.hotPage), 15);
 				} else {
-					func(Math.floor(ran * page), 15);
+					get_recommend(Math.floor(ran * scope.hotPage), 15);
 				}
 			}
-			//获取分类电影
-			get_page('/recommend/buycount', 1, scope.random_change);
-			get_page('/recommend/buycount', 2, scope.random_change);
-			scope.get_new(0, 48, true);
+			//获取热门电影
+			get_page('/recommend/buycount/0/15', 1, scope.random_change);
+			// get_page('/recommend/favor/' + scope.uid, 2, scope.random_change);
+			//猜你喜欢
+			get_guess();
+			//最新电影
+			scope.get_new(0, 32, true);
 			var max_page = 5, final_page;
 			scope.prev = function () {
                 if ((0 != scope.page_num[0]) && (scope.isActive == scope.page_num[0])) {
@@ -124,7 +115,7 @@ app.directive('homeDirective', ['request', '$state', function (request, $state) 
                 if (scope.isActive != 0) {
                     scope.isActive--;
                 }
-                scope.get_new(scope.isActive, 48);
+                scope.get_new(scope.isActive, 32);
             }
             scope.next = function () {
                 if ((scope.page != (scope.page_num[final_page - 1]) + 1) &&
@@ -136,7 +127,56 @@ app.directive('homeDirective', ['request', '$state', function (request, $state) 
                 if (scope.isActive != (scope.page - 1)) {
                     scope.isActive++;
                 }
-                scope.get_new(scope.isActive, 48);
+                scope.get_new(scope.isActive, 32);
+            }
+            //轮播器
+            var scroller = angular.element(document.getElementById('listfavor'));
+            var left = 0;
+            var tmp;
+            function move_left() {
+				setTimeout(function () {
+					if (left != -200) {
+						left--;
+            			scroller.css({'left': left + 'px'})
+            			move_left();
+					} else {
+						tmp = scope.recommondList[0];
+						scope.recommondList = scope.recommondList.slice(1)
+		        		scope.recommondList = scope.recommondList.concat(tmp);
+	            		console.log(scope.recommondList)
+	            		scope.scrollerFlag = true;
+					}
+				}, 1);
+            }
+            function move_right() {
+				setTimeout(function () {
+					if (left != 0) {
+						left++;
+            			scroller.css({'left': left + 'px'})
+            			move_right();
+					} else {
+	            		scope.scrollerFlag = true;
+					}
+				}, 1);            	
+            }
+            scope.left = function () {
+            	if (scope.scrollerFlag) {
+            		scope.scrollerFlag = false;
+	            	left = 0;
+            		move_left();
+            		scroller.css({'left': '0px'});
+            	}
+            }
+            scope.right = function () {
+            	if (scope.scrollerFlag) {
+            		scope.scrollerFlag = false;
+            		left = -200;
+					tmp = scope.recommondList[9];
+					scope.recommondList = scope.recommondList.slice(0, 9)
+	        		scope.recommondList = [tmp].concat(scope.recommondList);
+					scroller.css({'left': '-200px'})
+            		move_right();
+            	}
             }
 		}
 	}
