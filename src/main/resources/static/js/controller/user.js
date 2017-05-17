dc8543e660f99ab6a806989194100c653656cb5e
hanfeng <1024290349@qq.com>
2017-05-17 23:02:01 +0800

    修改user接口

--- src/main/resources/static/js/controller/user.js
+++ src/main/resources/static/js/controller/user.js
@@ -1,23 +1,27 @@
 app.controller('user', ['$scope', '$location', function ($scope, $location) {
+	//电影信息
 	$scope.userAbout = [];
-	//用于余额
-	$scope.count = null;
+	//用户信息
+	$scope.userInfo = {};
 	//修改余额
 	$scope.rechargeNum = null;
 	//修改密码
 	$scope.password = null;
-	//修改姓名
-	$scope.name = null;
 	//查看三种分类电影
 	$scope.chooseFilmFlag = 3;
 	//左侧菜单选择
 	$scope.chooseFlag = $location.path();
 }]);
-app.directive('userDirective', [function () {
+app.directive('userDirective', ['request', function (request) {
 	return {
 		restrict: 'AE',
 		link: function (scope, ele, attrs) {
 			scope.$emit('classify_change', false);
+			request.get('/usr/getUserinfo', function (res) {
+				if (res.code == 0) {
+					scope.userInfo = res.body.user;
+				}
+			})
 		}
 	}
 }]);
@@ -60,20 +64,13 @@ app.directive('userCountDirective', ['request', function (request) {
 	return {
 		restrict: 'AE',
 		link: function (scope, ele, attrs) {
-			request.get('url', function (res) {
-				if (res.code == 0) {
-					scope.count = res.body;
-				} else {
-					request.pop_up(res.msg);
-				}
-			})
 			scope.recharge = function () {
 				if (scope.rechargeNum) {
 					request.post('/usr/updateBalance', {
-						balance: scope.rechargeNum
+						balance: scope.rechargeNum.toString()
 					}, function (res) {
 						if (res.code == 0) {
-							scope.count = scope.rechargeNum;
+							scope.userInfo.balance = scope.rechargeNum;
 						}
 						request.pop_up(res.msg);
 					})
@@ -86,23 +83,16 @@ app.directive('userInfoDirective', ['request', function (request) {
 	return {
 		restrict: 'AE',
 		link: function (scope, ele, attrs) {
-			scope.changeName = function () {
-				if (scope.name) {
-					request.post('/usr/updateProfile', {
-						name: scope.name
-					}, function (res) {
-						request.pop_up(res.msg);
-					})
-				} else {
-					request.pop_up('不能为空');
-				}
-			}
 			scope.changePassword = function () {
 				if (scope.password) {
 					request.post('/usr/updateProfile', {
 						password: scope.password
 					}, function (res) {
-						request.pop_up(res.msg);
+						if (res.code == 0) {
+							scope.logout();
+						} else {
+							request.pop_up(res.msg);
+						}
 					})
 				} else {
 					request.pop_up('不能为空');
