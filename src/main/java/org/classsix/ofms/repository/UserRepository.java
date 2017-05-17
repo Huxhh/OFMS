a07d81389a31f99f0a380858e2eeba3960e6ec81
jianglilinx <172045149@qq.com>
2017-05-17 22:35:58 +0800

    添加了评分 待添加：1、权限管理

--- src/main/java/org/classsix/ofms/repository/UserRepository.java
+++ src/main/java/org/classsix/ofms/repository/UserRepository.java
@@ -9,19 +9,23 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by jiang on 2017/5/3.
  * 面向运气，面向心情，面向Bug。
  */
+@Transactional
 public interface UserRepository extends JpaRepository<User, Integer> {
     User findByUserName(String userName);
     User findByUserNameAndMail(String userName,String mail);
 
 
+
     @Modifying
     @Query("update User u set u.password=:password where u.id=:id")
     int update(@Param("id") Integer id,@Param("password") String password);
+
 
     @Modifying
     @Query("update User u set u.balance=:balance where u.id=:id")
